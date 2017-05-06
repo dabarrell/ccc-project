beb6c15b9d6b8a0d3833af2ba2559f2baf5eccf8
bkot <bkoteski88@gmail.com>
2017-05-06 17:40:39 +1000

    finsys update
    retrieve stats based on recent tweets only.

---
+++ finsys/src/finsys.py
@@ -0,0 +1,50 @@
+
+import couchdb
+import sys, os
+import datetime as dt
+import time
+
+def main():
+    db = couchdb.Server('http://admin:password@115.146.93.197:5984/')['fin_tweets']
+
+    stats = {
+        'eurusd':{'mentions':0, 'buys':0, 'sells':0},
+        'audusd':{'mentions':0, 'buys':0, 'sells':0},
+        'usdjpy':{'mentions':0, 'buys':0, 'sells':0},
+        'usdcad':{'mentions':0, 'buys':0, 'sells':0},
+        'gbpusd':{'mentions':0, 'buys':0, 'sells':0},
+        'usdchf':{'mentions':0, 'buys':0, 'sells':0},
+    }
+
+    for row in db.view('_all_docs', include_docs=True):
+        if row.doc['lang'] == 'en':
+            tweet_time = created_at_to_datetime(row.doc['created_at'])
+            if (dt.datetime.now() - tweet_time).seconds / 60 < 60:
+                print tweet_time
+                t_text = row.doc['text'].encode('ascii','ignore').lower()
+                for key in stats:
+                    if key in t_text:
+                        stats[key]['mentions'] += 1
+                        if 'buy' in t_text or 'long' in t_text:
+                            stats[key]['buys'] += 1
+                        elif 'sell' in t_text or 'short' in t_text:
+                            stats[key]['sells'] += 1
+    print stats
+
+
+def created_at_to_datetime(string):
+    f = '%a %b %d %H:%M:%S %Y'
+    assert '+0000' in string, 'twitter date unexpected utc offset'
+    s = string.replace(' +0000','')
+    tweet_time = dt.datetime.strptime(s,f)
+    # adjust tweet time for utc offset
+    return tweet_time + dt.timedelta(seconds=get_utc_offset().total_seconds())
+
+
+def get_utc_offset():
+    ts = time.time()
+    return (dt.datetime.fromtimestamp(ts) - dt.datetime.utcfromtimestamp(ts))
+
+
+if __name__ == '__main__':
+    main()
