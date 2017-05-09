81c97d340098f074e9c1f6466ff1f98972d65211
bkot <bkoteski88@gmail.com>
2017-05-09 13:42:00 +1000

    stream.py update
    the twitter stream script can now be invoked with command line args, see
    -h for useage.

--- tweepy/stream.py
+++ tweepy/stream.py
@@ -1,14 +1,52 @@
+'''
+python stream.py 
+	--host localhost
+	--port 5984
+	--member admin 
+	--pw password 
+	--dbname test1 
+	--keywords audusd,eurusd 
+	--debug
+'''
+
 import tweepy
 import json
 import couchdb
 import time
+from argparse import ArgumentParser
+
+parser = ArgumentParser(description='Start up a twitter stream and store tweets in a CouchDB')
+parser.add_argument('--ckey',help='Consumer Key for twitter',required=True)
+parser.add_argument('--csecret',help='Consumer Secret for twitter',required=True)
+parser.add_argument('--atoken',help='Access Token for twitter',required=True)
+parser.add_argument('--asecret',help='Access Token Secret for twitter',required=True)
+parser.add_argument('--port', default=5984, help='CouchDB port number (%(default)s)', type=int)
+parser.add_argument('--host', help='CouchDB host address (%(default)s)', default='localhost', type=str)
+parser.add_argument('--member', help='CouchDB member name', default='', type=str)
+parser.add_argument('--pw', help='CouchDB member pw', default='', type=str)
+parser.add_argument('--dbname', help='CouchDB database name', required=True, type=str)
+parser.add_argument('--keywords',help='(Optional) Key words for twitter, e.g., keyword1,keyword2,...')
+parser.add_argument('--debug', help='Print debug messages to screen', action='store_true')
+args = parser.parse_args()
 
 # Authentication details. To  obtain these visit dev.twitter.com
-consumer_key = ''
-consumer_secret = ''
-access_token = ''
-access_token_secret = ''
-tweetdb = couchdb.Server('http://user:user_pw@ip:5984/')['db_name']
+consumer_key = args.ckey
+consumer_secret = args.csecret
+access_token = args.atoken
+access_token_secret = args.asecret
+
+# try and connect to the couchdb instance
+try:
+	if args.member is '':
+		server = 'http://' + args.host + ':' + str(args.port) + '/'
+	else:
+		server = 'http://'+args.member+':'+args.pw+'@'+args.host+':'+str(args.port)+'/'
+	# db = pycouchdb.Server(server).database(args.dbname)
+	tweetdb = couchdb.Server(server)[args.dbname]
+	if args.debug is True: print('connection successful')
+except Exception as e:
+	print('Connection unsuccessful')
+	sys.exit()
 
 # This is the listener, resposible for receiving data
 class StdOutListener(tweepy.StreamListener):
@@ -20,17 +58,15 @@ class StdOutListener(tweepy.StreamListener):
         # print json.dumps(decoded['user']['screen_name'], indent=4)
         # print json.dumps(decoded,indent=4)
         try:
+        	# set the doc id
             doc = {
-                '_id':decoded['id_str'],
-                'text':decoded['text'],
-                'coordinates':decoded['coordinates'],
-                'user_name':decoded['user']['screen_name']
+                '_id':decoded['id_str']
             }
-            if decoded['place'] is not None:
-                doc['place'] = decoded['place']['name']
-            else:
-                doc['place'] = None
-            print doc
+            # update the rest of the json object into doc
+            doc.update(decoded)
+            if args.debug is True:
+            	print doc
+            # put the doc in the db
             print tweetdb.save(doc)
             print ''
         except Exception as e:
@@ -55,8 +91,12 @@ if __name__ == '__main__':
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
 
-    key_word = 'australia'
-    print "Showing all new tweets for " + key_word
+    if args.keywords == None:
+    	print 'Streaming tweets from Australia.'
+    	print ''
+    else:
+    	key_words = args.keywords.split(',')
+    	print 'Streaming tweets with ', key_words
 
     while True:
         try:
@@ -64,8 +104,10 @@ if __name__ == '__main__':
             # In this example follow #programming tag
             # For more details refer to https://dev.twitter.com/docs/streaming-apis
             stream = tweepy.Stream(auth, l)
-            # stream.filter(track=[key_word,'melbourne'])
-            stream.filter(locations=[110.95,-54.83,159.29,-11.35])
+            if args.keywords == None:
+            	stream.filter(locations=[110.95,-54.83,159.29,-11.35])
+            else:
+            	stream.filter(track=key_words)
         except:
             print 'sleeping for 15 mins'
             print ''
